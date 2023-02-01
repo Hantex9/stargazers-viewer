ce02abfe713d21d1245026ebd07c46f786123de7
Alessandro Izzo <34343582+Hantex9@users.noreply.github.com>
2023-02-02 00:04:40 +0100

    added the pre-loading assets at app startup

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, View } from 'native-base';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { AntDesign, MaterialIcons } from '@expo/vector-icons';
+
 import { AppNavigator } from './navigation/AppNavigator';
+
+// Keep the splash screen visible while fetching resources
+SplashScreen.preventAutoHideAsync();
 
 // Initializing the Query client used for the integration with React Query
 const queryClient = new QueryClient();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts
+        await Font.loadAsync(AntDesign.font);
+        await Font.loadAsync(MaterialIcons.font);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application that is ready and have to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <NativeBaseProvider>
-      <QueryClientProvider client={queryClient}>
-        <StatusBar style="light" />
-        <AppNavigator />
-      </QueryClientProvider>
+      <View flex={1} onLayout={onLayoutRootView}>
+        <QueryClientProvider client={queryClient}>
+          <StatusBar style="light" />
+          <AppNavigator />
+        </QueryClientProvider>
+      </View>
     </NativeBaseProvider>
   );
 }
